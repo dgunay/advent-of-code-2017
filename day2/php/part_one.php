be0fa2b65b79893d8efdea1dab9a0e18969206af
dgunay <devingunay@gmail.com>
2017-12-02 01:33:22 -0800

    Solve part 1 and 2

--- day2/php/part_one.php
+++ day2/php/part_one.php
@@ -10,8 +10,26 @@
 
 $fp_in = fopen('../input.txt', 'r');
 
+$checksum = 0;
 while($line = fgets($fp_in)) {
   preg_match_all('/\d+/', $line, $matches);
-  print_r($matches;
-  exit;)
+  $values = $matches[0];
+
+  $smallest_value = $values[0];
+  $largest_value = $values[0];
+
+  foreach ($values as $value) {
+    if ($value < $smallest_value) {
+      $smallest_value = $value;
+    }
+
+    if ($value > $largest_value) {
+      $largest_value = $value;
+    }
+  }
+
+  $difference = $largest_value - $smallest_value;
+  $checksum += $difference;
 }
+
+echo $checksum;
