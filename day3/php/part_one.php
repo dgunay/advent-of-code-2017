62089abc437f5d7f88d493f562dada4fe39e4aed
dgunay <devingunay@gmail.com>
2017-12-03 13:51:16 -0800

    Begin solving part 1

---
+++ day3/php/part_one.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * 17  16  15  14  13
+ * 18   5   4   3  12
+ * 19   6   1   2  11
+ * 20   7   8   9  10
+ * 21  22  23---> ...
+ * 
+ * How many steps away is our number, if you move only up down left or right?
+ */
+
+$input = file_get_contents('../input.txt');
+
+var_dump($input);
