333c74eebe0bd369655f1d9833dd17025290c25e
Devin Gunay <devingunay@gmail.com>
2018-01-02 14:47:33 -0800

    Begin work on day 9

---
+++ day9/php/part_one.php
@@ -0,0 +1,29 @@
+<?php
+
+
+/**
+ * A group is enclosed with {}
+ * 
+ * Within them, are 0 or more comma-separated things:
+ * - another group
+ * - or garbage. 
+ * 
+ * Garbage is enclosed by <>.
+ * Within garbage, {} can appear, and < has no meaning.
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+function solve(resource $stream) {
+	
+}
